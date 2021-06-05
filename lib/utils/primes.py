9e0b60d5c3a014510be69aa82fbf66e4a8124a8f
jbnerd <abhivjoshi.aj@gmail.com>
2021-06-06 00:58:41 +0530

    Improve tests for prime factorization logic

--- lib/utils/primes.py
+++ lib/utils/primes.py
@@ -5,7 +5,10 @@ from lib.sequence_generators import PrimeNumberSequenceGenerator
 
 
 class PrimeFactorizationHelper:
-    """Utilities related to prime factors and prime factorization of numbers."""
+    """
+    Helper class for prime factorization of numbers. The main logic of this class uses a cache of prime numbers which
+    are generated before computing the factorization.
+    """
 
     _primes: List[int] = []
     curr_upper_bound: int = -1
@@ -13,39 +16,47 @@ class PrimeFactorizationHelper:
     @classmethod
     def update_prime_cache(cls, upper_bound: int):
         cls.curr_upper_bound = upper_bound
-        cls._primes = cls._prepare_prime_table(upper_bound)
+        cls._prepare_prime_cache(upper_bound)
 
-    @staticmethod
-    def _prepare_prime_table(num: int) -> List[int]:
-        upper_bound = int(sqrt(num) + 1)
-        return PrimeNumberSequenceGenerator.generate(upper_bound)
+    @classmethod
+    def _prepare_prime_cache(cls, num: int):
+        cls.curr_upper_bound = int(sqrt(num) + 1)
+        cls._primes = PrimeNumberSequenceGenerator.generate(cls.curr_upper_bound)
 
     @classmethod
     def prime_factorization(cls, num: int) -> Tuple[List[int], List[int]]:
+        """
+        The search space is pruned by using the fact that there can only be one prime factor greater than the square
+        root of the number.
+        """
         if cls.curr_upper_bound < int(sqrt(num) + 1):
-            cls._primes = cls._prepare_prime_table(num)
+            cls._prepare_prime_cache(num)
 
         factors, exponents, limit = [], [], sqrt(num)
         for prime in cls._primes:
-            if prime >= sqrt(num):
+            if prime > limit:
+                if num != 1:
+                    factors.append(int(num))
+                    exponents.append(1)
                 break
-            exponent = 0
-            while num % prime == 0:
-                exponent, num = exponent + 1, num / prime
+            num, exponent = cls._process_one_iteration(num, prime)
             if exponent != 0:
                 factors.append(prime)
                 exponents.append(exponent)
-
-        # There can only be one prime factor greater than the sqrt of the number.
-        if num != 1:
-            factors.append(int(num))
-            exponents.append(1)
-
         return factors, exponents
+
+    @staticmethod
+    def _process_one_iteration(num: int, prime: int) -> Tuple[int, int]:
+        exponent = 0
+        while num % prime == 0:
+            exponent, num = exponent + 1, num / prime
+        return num, exponent
 
 
 def prime_factorization(num: int) -> Tuple[List[int], List[int]]:
     """A safe upper bound for the cache of prime numbers to factorize a 32-bit integer is 2^16."""
+    if num < 1:
+        raise ValueError("Provide a positive integer for factorization.")
     if PrimeFactorizationHelper.curr_upper_bound < 0:
         PrimeFactorizationHelper.update_prime_cache(2 ** 16)
     return PrimeFactorizationHelper.prime_factorization(num)
--- lib/utils/test_primes.py
+++ lib/utils/test_primes.py
@@ -19,18 +19,6 @@ def test_not_is_prime(num: int):
     assert not is_prime(num)
 
 
-@pytest.mark.parametrize('num, correct_factors', [
-    (28, [2, 7]),
-    (39, [3, 13]),
-    (13195, [5, 7, 13, 29]),
-    (111111, [3, 7, 11, 13, 37]),
-    (600851475143, [71, 839, 1471, 6857])
-])
-def test_get_prime_factors(num: int, correct_factors: List[int]):
-    predicted_factors = prime_factors(num)
-    assert predicted_factors == correct_factors
-
-
 @pytest.mark.parametrize('n, prime', [
     (6, 13),
     (100, 541)
@@ -39,11 +27,44 @@ def test_get_nth_prime(n, prime):
     assert prime == get_nth_prime(n)
 
 
-@pytest.mark.parametrize('n, factors, exponents', [
+@pytest.mark.parametrize('n, correct_factors, correct_exponents', [
+    (1, [], []),
+    (2, [2], [1]),
+    (13, [13], [1]),
     (28, [2, 7], [2, 1]),
+    (32, [2], [5]),
+    (100, [2, 5], [2, 2]),
+    (9085, [5, 23, 79], [1, 1, 1]),
     (76576500, [2, 3, 5, 7, 11, 13, 17], [2, 2, 3, 1, 1, 1, 1])
 ])
-def test_prime_factorization(n, factors, exponents):
+def test_prime_factorization(n, correct_factors, correct_exponents):
     predicted_factors, predicted_exponents = prime_factorization(n)
-    assert predicted_factors == factors
-    assert predicted_exponents == exponents
+    assert predicted_factors == correct_factors
+    assert predicted_exponents == correct_exponents
+
+
+def test_prime_factorization_given_negative_number():
+    with pytest.raises(ValueError) as exc:
+        prime_factorization(-1)
+    assert str(exc.value) == "Provide a positive integer for factorization."
+
+
+@pytest.mark.parametrize('num, correct_factors', [
+    (1, []),
+    (2, [2]),
+    (13, [13]),
+    (39, [3, 13]),
+    (100, [2, 5]),
+    (13195, [5, 7, 13, 29]),
+    (111111, [3, 7, 11, 13, 37]),
+    (600851475143, [71, 839, 1471, 6857])
+])
+def test_prime_factors(num: int, correct_factors: List[int]):
+    predicted_factors = prime_factors(num)
+    assert predicted_factors == correct_factors
+
+
+def test_prime_factors_given_negative_number():
+    with pytest.raises(ValueError) as exc:
+        prime_factors(-1)
+    assert str(exc.value) == "Provide a positive integer for factorization."
