25c7633ddec971dd8b541a37f7bd4290d6312e10
jbnerd <abhivjoshi.aj@gmail.com>
2021-06-07 01:11:03 +0530

    Refactor 13 for readability

--- solutions/13.py
+++ solutions/13.py
@@ -1,7 +1,7 @@
-"""Plan of attack:
-    v1: (0.000726 seconds)
-        Use addition of large numbers using string representation and optimize on word-size of the processor for
-        maximum performance.
+"""
+Average execution times:
+    v1: (0.000577 seconds)
+Answer: 5537376230
 """
 from functools import reduce
 
@@ -10,8 +10,12 @@ from lib.utils.generic import Timer
 from lib.utils.numeric import large_number_sum
 
 
-@Timer(name='decorator')
+@Timer()
 def execute_v1():
+    """
+    Logic for addition of large numbers using string representation is used. Optimization is done based on the word-size
+    of the processor for maximum performance.
+    """
     with open('solutions/data/13.txt') as infile:
         data = infile.read().strip().split('\n')
     print(reduce(lambda a, b: large_number_sum(a, b, 64), data)[:10])
