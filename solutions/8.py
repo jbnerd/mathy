a420622f5b327c3c020cffea5dbf1864a2f2cfd7
jbnerd <abhivjoshi.aj@gmail.com>
2021-06-06 23:01:17 +0530

    Refactor 8 for readability

--- solutions/8.py
+++ solutions/8.py
@@ -1,17 +1,23 @@
-"""Plan of attack:
+"""
+Average execution times:
     v1: (0.002185 seconds)
-        Collect digits window-wise and keep a track of the maximum product.
+Answer: 23514624000
 """
 import _init_paths
 from lib.utils.strings import character_wise_product
 from lib.utils.generic import Timer
 
 
-@Timer(name='decorator')
-def execute_v1():
+def read_data():
     with open('solutions/data/8.txt') as infile:
         data = ''.join(infile.read().strip().split('\n'))
+    return data
 
+
+@Timer()
+def execute_v1():
+    """Collect digits window-wise and keep a track of the maximum product."""
+    data = read_data()
     max_prod = 0
     for i, _ in enumerate(data[:-13]):
         max_prod = max(max_prod, character_wise_product(data[i:i+13]))
