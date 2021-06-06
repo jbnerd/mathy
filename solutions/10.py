696dbfbf443cb992789d10b1b3f96e0c659f9885
jbnerd <abhivjoshi.aj@gmail.com>
2021-06-06 23:18:41 +0530

    Remove dependency on get_primes call

--- solutions/10.py
+++ solutions/10.py
@@ -7,13 +7,13 @@ from functools import reduce
 
 import _init_paths
 from lib.utils.generic import Timer
-from lib.utils.primes import get_primes
+from lib.sequence_generators import PrimeNumberSequenceGenerator
 
 
 @Timer(name='decorator')
 def execute_v1():
     """Generate the primes using sieve and add."""
-    print(reduce(lambda a, b: a + b, get_primes(2000000)))
+    print(reduce(lambda a, b: a + b, PrimeNumberSequenceGenerator.generate(2000000)))
 
 
 if __name__ == "__main__":
