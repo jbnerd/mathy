3dc7d4dea38e2452827c874599b7d75a6beba2d0
jbnerd <abhivjoshi.aj@gmail.com>
2021-06-10 22:39:40 +0530

    Refactor 15 for readability

--- solutions/15.py
+++ solutions/15.py
@@ -1,7 +1,7 @@
-"""Plan of attack:
-    v1: (0.000034 seconds)
-        The number of lattice paths in an m x n grid is given by C(m+n, n) or C(m+n, m). Where C(a, b) represents the
-        number of ways in which b distinct objects can be selected from a distinct objects.
+"""
+Average execution times:
+    v1: (0.000020 seconds)
+Answer: 137846528820
 """
 import _init_paths
 from lib.utils.generic import Timer
@@ -10,6 +10,10 @@ from lib.utils.numeric import num_combinations
 
 @Timer(name='decorator')
 def execute_v1():
+    """
+    The number of lattice paths in an m x n grid is given by C(m+n, n) or C(m+n, m). Where C(a, b) represents the
+    number of ways in which b distinct objects can be selected from a distinct objects.
+    """
     print(num_combinations(40, 20))
 
 
