e17f56f6544ebf8d6e737ee930bfd440da69d3a4
jbnerd <abhivjoshi.aj@gmail.com>
2022-03-20 22:21:55 -0400

    Add digit to word representation functionality
    - Current support is provided only for integers in the range of
      [1, 1000].

--- euler/utils/strings.py
+++ euler/utils/strings.py
@@ -5,6 +5,49 @@ This file provides utilities for operations on numbers represented as strings.
 import functools
 
 
+class NumToWord:
+    """Converts a natural number into its english representation"""
+
+    _num_to_word = {
+        1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', 6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine', 10: 'Ten',
+        11: 'Eleven', 12: 'Twelve', 13: 'Thirteen', 14: 'Fourteen', 15: 'Fifteen', 16: 'Sixteen', 17: 'Seventeen',
+        18: 'Eighteen', 19: 'Nineteen', 20: 'Twenty', 30: 'Thirty', 40: 'Forty', 50: 'Fifty', 60: 'Sixty',
+        70: 'Seventy', 80: 'Eighty', 90: 'Ninety'
+    }
+
+    @classmethod
+    def convert(cls, num: int) -> str:
+        if num <= 0 or num > 1000:
+            raise ValueError('Only integers in [1, 1000] are supported for word representation.')
+        elif num <= 99:
+            return cls._handle_till_tens(num)
+        elif num <= 999:
+            return cls._handle_till_hundreds(num)
+        else:
+            return 'One Thousand'
+
+    @classmethod
+    def _handle_till_tens(cls, num: int) -> str:
+        if num == 0:
+            return ''
+        elif num <= 20:
+            return cls._num_to_word[num]
+        else:
+            tens = cls._num_to_word[num - (num % 10)]
+            ones = cls._num_to_word.get(num % 10, '')
+            return '-'.join([tens, ones])
+
+    @classmethod
+    def _handle_till_hundreds(cls, num: int) -> str:
+        hundreds = cls._handle_hundreds_digit(num)
+        tens = cls._handle_till_tens(num % 100)
+        return ' and '.join([hundreds, tens]) if tens != '' else hundreds
+
+    @classmethod
+    def _handle_hundreds_digit(cls, num: int) -> str:
+        return cls._num_to_word[int(num / 100)] + ' Hundred'
+
+
 def is_palindrome_number(num: int) -> bool:
     num = str(num)
     return num == num[::-1]
--- tests/utils/test_strings.py
+++ tests/utils/test_strings.py
@@ -1,16 +1,38 @@
 import pytest
 
-from euler.utils.strings import is_palindrome_number
+from euler.utils.strings import is_palindrome_number, NumToWord
 
 
-@pytest.mark.parametrize("palindrome", [
+@pytest.mark.parametrize('num, word', [
+    (6, 'Six'),
+    (20, 'Twenty'),
+    (56, 'Fifty-Six'),
+    (100, 'One Hundred'),
+    (556, 'Five Hundred and Fifty-Six'),
+    (1000, 'One Thousand')
+])
+def test_num_to_word_acceptable_range(num, word):
+    predicted_word = NumToWord.convert(num)
+    assert predicted_word == word
+
+
+@pytest.mark.parametrize('num', [
+    0, -10, 1001, 10000
+])
+def test_num_to_word_out_of_acceptable_range(num):
+    with pytest.raises(ValueError) as exc:
+        _ = NumToWord.convert(num)
+    assert str(exc.value) == 'Only integers in [1, 1000] are supported for word representation.'
+
+
+@pytest.mark.parametrize('palindrome', [
     9, 101, 9119, 12344321, 8769678,
 ])
 def test_is_palindrome(palindrome):
     assert is_palindrome_number(palindrome)
 
 
-@pytest.mark.parametrize("non_palindrome", [
+@pytest.mark.parametrize('non_palindrome', [
     10, 193401
 ])
 def test_not_is_palindrome(non_palindrome):
